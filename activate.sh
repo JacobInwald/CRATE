44983a4c3e5e358a5473da33ff5da7dd89aed06a
Jacob Inwald <jacob.inwald@hotmail.com>
2025-01-14 13:01:48 +0000

    final refactor

--- activate.sh
+++
@@ -1,4 +0,0 @@
-source .env/bin/activate
-export PATH="./scripts:$PATH"
-export AFL_SKIP_CPUFREQ=1
-export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=0
--- src/setup.py
+++
@@ -1,4 +0,0 @@
-# TODO: Write a script to setup the available tests and create the directories
-from seeds import gen_seeds
-
-gen_seeds()
